b41ae9843ac90817a303e92ea46cb422600c9f01
CyCoder <ptuan917@gmail.com>
2016-01-25 18:05:12 -0500

    made the contact form work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,14 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(5)
   end
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+  		:to => 'ptuan917@gmail.com',
+  		:subject => "Bike Berlin, #{@name} sent you a message!",
+  		:body => @message).deliver_now
+  end
 end
