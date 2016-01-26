bcae463b7eb877a7a4946f40999cc824805d68ac
CyCoder <ptuan917@gmail.com>
2016-01-25 22:52:00 -0500

    changed root route and added a new redirect

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "ptuan917@gmail.com"
+  layout 'mailer'
+end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,17 @@
+class UserMailer < ApplicationMailer
+	default from: "ptuan917@gmail.com"
+
+	def contact_form(email, name, message)
+		@message = message
+			mail(:from => email,
+				   :to => 'ptuan917@gmail.com',
+  		     :subject => "Bike Berlin, #{@name} sent you a message!")
+	end
+
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		UserMailer.contact_form(@email, @name, @message).deliver_now
+	end
+end
