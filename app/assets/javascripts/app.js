dd8ab8b3d3013084aac7c0d6a924df9ed6352eeb
CyCoder <ptuan917@gmail.com>
2016-04-01 17:31:06 -0400

    revised Ajax

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,5 +6,9 @@ $(document).on('ready page:load', function() {
 
 app.controller('OrdersCtrl', ['$scope', function($scope){
   // Here will be all code belonging to this controller
-
+  $scope.orders =[
+    {id: 1, total: 24, product_id: 1, user_id: 1},
+    {id: 2, total: 7, product_id: 1},
+    {id: 3, total: 42, product_id: 1, user_id: 1}
+  ];
 }]);
